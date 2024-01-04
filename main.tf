287dc62b11638ba7c9eba4f65c751fa55c7dd2d9
Tyler Allen <tyler.allen@hashicorp.com>
2024-01-04 11:45:54 +0000

    Change to values

--- main.tf
+++ main.tf
@@ -90,9 +90,9 @@ resource "aws_instance" "consul_client" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.small"
   associate_public_ip_address = true
-  subnet_id                   = data.tfe_outputs.vpc-deployment.outputs.vpc_subnet_id
+  subnet_id                   = data.tfe_outputs.vpc-deployment.values.vpc_subnet_id
   vpc_security_group_ids = [
-    data.tfe_outputs.vpc-deployment.outputs.aws_security_group_id,
+    data.tfe_outputs.vpc-deployment.values.aws_security_group_id,
     aws_security_group.allow_ssh.id
   ]
   # key_name = aws_key_pair.consul_client.key_name
@@ -107,7 +107,7 @@ resource "aws_instance" "consul_client" {
         service_name = "consul",
         service_cmd  = "/usr/bin/consul agent -data-dir /var/consul -config-dir=/etc/consul.d/",
       })),
-      vpc_cidr = data.tfe_outputs.vpc-deployment.outputs.vpc_cidr_block
+      vpc_cidr = data.tfe_outputs.vpc-deployment.values.vpc_cidr_block
     })),
   })
 
